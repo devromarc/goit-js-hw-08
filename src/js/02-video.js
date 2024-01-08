d4a2354e82e259103038681749a9bf7aaeac7c49
devromarc <rmrcslvdr@gmail.com>
2024-01-09 03:58:36 +0800

    task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,12 +5,13 @@ import throttle from 'lodash.throttle';
 // Initialize the player in the script file as described in the pre-existing player section, but note that you have added the player as an npm package, not via CDN.
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const onHandleThrottle = data => {
-  const timeInSec = data.seconds;
-  //   console.log(timeInSec);
+  const getTime = data.seconds;
+  //   console.log(getTime);
   //   Save playback time to local storage. Let the key for the storage be the "videoplayer-current-time" string.
-  localStorage.setItem('videoplayer-current-time', timeInSec);
+  localStorage.setItem(STORAGE_KEY, getTime);
 };
 // Read the documentation of the on() method and start tracking the timeupdate event - playback time update.
 // Add the lodash.throttle library to the project and make sure that the playback time is updated in the storage once a second or less frequent.
@@ -18,7 +19,7 @@ player.on('timeupdate', throttle(onHandleThrottle, 1000));
 
 // When reloading the page, use the setCurrentTime() method to resume playback from the saved position.
 player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
   .then(function (seconds) {
     // seconds = the actual time that the player seeked to
   })
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const formData = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
+
+// TRACK THE INPUT THAT WILL SAVE TO THE LOCAL STORAGE
+const onHandleThrottle = () => {
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({ email: emailInput.value, message: messageInput.value })
+  );
+};
+
+// FUNCTION THAT HANDLES THE SUBMIT EVENT
+const onHandleSubmit = event => {
+  event.preventDefault();
+  const storageData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(storageData);
+  localStorage.removeItem(STORAGE_KEY);
+  event.target.reset();
+};
+
+// CONDITION IF THERES A DATA THAT WAS SAVED IN THE LOCAL STORAGE
+if (localStorage.getItem(STORAGE_KEY)) {
+  const { email, message } = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  emailInput.value = email;
+  messageInput.value = message;
+}
+
+formData.addEventListener('input', throttle(onHandleThrottle, 500));
+formData.addEventListener('submit', onHandleSubmit);
